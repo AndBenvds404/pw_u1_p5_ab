3340d0ca9ec8650fde380b25814b06506b144621
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-12-13 19:44:27 -0500

    taller 15

--- js/app.js
+++ js/app.js
@@ -1,15 +1,64 @@
-console.log('objeto Vue')
-console.log(Vue)
+console.log("elementos Vue");
+console.log(Vue);
 
+const estudiantes = [{nombre:"Andres", apellido: "Benavides"},
+{nombre:"Pepe", apellido: "Grisho"},
+{nombre:"Vegeta", apellido: "7777"},
+{nombre:"Yiu", apellido: "Fuentes"},
+{nombre:"Toni", apellido: "Nadal"}
+]
+
+console.log(estudiantes)
+console.table()
 
 const app = Vue.createApp({
-    template: `
-    <h1> Hola Mundo </h1>
-    <p>Desde App.js</p>
-    <h1>{{1+1}}</h1>
-    <p>{{[1,2,3,4,5,6,7]}}</p>
-    <p>{{true?'VERDADERO':'FALSO'}}</p>
-    `    
-})
+    // Options API
+    //template:`
+    //<h1>Hola mundo</h1>
+    //<p>Desde app.js</p>
+    //<h6>{{1+1}}</h6>
+    //<p>{{[1, 2, 3, 4, 5, 6, 7, 8]}}</p>
+    //<p>{{false?'Verdadero': 'Falso'}}</p>
+    //`
+    methods:{
+        cambiarMensaje() {
+            console.log("Se esta cambiando el  mensaje")
+            console.log(this.mensaje)
+            this.mensaje='Valor cambiado'
+        },
+        cambiarNumero() {
+            console.log(this.valor)
+            this.valor++;
+        },
+        agregar(){
+        
+            console.log('hola')
+            this.lista.push({nombre:this.nombre, apellido: this.apellido})
+        },
 
-app.mount('#ProyectoVue')
+        presionandoTecla(event){
+            console.log('presionando tecla')
+            console.log(event.charCode)
+            const enter = event.charCode
+            if (enter === 13){
+                this.agregar()
+            }
+        }
+    },
+    watch:{
+
+    },
+    data(){
+        return {
+            mensaje:'Hola mundo desde Vue.JS',
+            valor:100,
+            lista: estudiantes,
+            
+            nombre: null,
+            apellido: null
+            
+        }
+    }
+});
+
+app.mount('#miApp')
